88fdbe06fa5333c82af14d509564c63f34b57e39
MOHAMMED YAMEEN S M <109512433+yamyy123@users.noreply.github.com>
2023-03-01 20:06:15 +0530

    Create happy number.cpp

---
+++ happy number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int result(int n)
+    {
+        int sum=0,r;
+        while(n)
+        {
+            r=n%10;
+            sum=sum+(r*r);
+            n/=10;
+        }
+        return sum;
+        
+    }
+
+    bool isHappy(int n)
+     {  
+         int first,second; 
+    first=second=n;
+   do{
+    first=result(first);
+    second=result(second);
+    second=result(second);
+    }while(first!=second);
+    if(first==1)
+    {
+        return true;
+    }
+    else
+    return false;
+    }
+};
